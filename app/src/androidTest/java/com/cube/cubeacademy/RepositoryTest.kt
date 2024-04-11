86fdbc8755383de2f13e6ba84ddc94dfdfa97903
Chloe <cgreenstreet01@gmail.com>
2024-04-11 20:50:01 +0100

    Passing getNominationsTest

--- app/src/androidTest/java/com/cube/cubeacademy/RepositoryTest.kt
+++ app/src/androidTest/java/com/cube/cubeacademy/RepositoryTest.kt
@@ -29,12 +29,13 @@ class RepositoryTest {
 
     @Test
     fun getNominationsTest() = runBlocking {//added to tests on suspend functions
-        val mockApiService = MockApiService();
-        val data = "data";
+        val result = Repository(MockApiService()).getAllNominations()
+        val expected = listOf(
+            Nomination("1", "1", "reason", "process", "2023-10-11", "2023-11-11"),
+            Nomination("2", "2", "reason", "process", "2023-10-11", "2023-11-11"),
+            Nomination("1", "3", "reason", "process", "2023-10-11", "2023-11-11"),
+            Nomination("2", "4", "reason", "process", "2023-10-11", "2023-11-11"),)
 
-        val result = Repository(mockApiService).getAllNominations()
-        val expected = listOf(Nomination(data, data, data, data, data, data))
-//<[Nomination(nominationId=1, nomineeId=1, reason=reason, process=process, dateSubmitted=2023-10-11, closingDate=2023-11-11), Nomination(nominationId=2, nomineeId=2, reason=reason, process=process, dateSubmitted=2023-10-11, closingDate=2023-11-11), Nomination(nominationId=1, nomineeId=3, reason=reason, process=process, dateSubmitted=2023-10-11, closingDate=2023-11-11), Nomination(nominationId=2, nomineeId=4, reason=reason, process=process, dateSubmitted=2023-10-11, closingDate=2023-11-11)]>
         assertEquals(expected,result);
     }
 
