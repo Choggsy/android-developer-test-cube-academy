e29a541516da5cf716d584eec1cd3bb8367b364f
Chloe <cgreenstreet01@gmail.com>
2024-04-11 21:36:43 +0100

    Nomination creates with incrementedNominationID (not right, commited to document logic confusion)

--- app/src/androidTest/java/com/cube/cubeacademy/RepositoryTest.kt
+++ app/src/androidTest/java/com/cube/cubeacademy/RepositoryTest.kt
@@ -3,6 +3,7 @@ package com.cube.cubeacademy
 import com.cube.cubeacademy.di.MockApiService
 import com.cube.cubeacademy.lib.di.Repository
 import com.cube.cubeacademy.lib.models.Nomination
+import com.cube.cubeacademy.lib.models.Nominee
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.runBlocking
@@ -12,10 +13,18 @@ import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
 
+private const val ID1 = "1"
+
+private const val ID2 = "2"
+
+private const val ID3 = "3"
+
+private const val REASON = "reason"
+
+private const val PROCESS = "process"
+
 @HiltAndroidTest
 class RepositoryTest {
-    // FIXME Can use the MockApiSerivce to run the repository Tests
-
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
@@ -31,21 +40,32 @@ class RepositoryTest {
     fun getNominationsTest() = runBlocking {//added to tests on suspend functions
         val result = Repository(MockApiService()).getAllNominations()
         val expected = listOf(
-            Nomination("1", "1", "reason", "process", "2023-10-11", "2023-11-11"),
-            Nomination("2", "2", "reason", "process", "2023-10-11", "2023-11-11"),
-            Nomination("1", "3", "reason", "process", "2023-10-11", "2023-11-11"),
-            Nomination("2", "4", "reason", "process", "2023-10-11", "2023-11-11"),)
+            Nomination(ID1, ID1, REASON, PROCESS, "2023-10-11", "2023-11-11"),
+            Nomination(ID2, ID2, REASON, PROCESS, "2023-10-11", "2023-11-11"),
+            Nomination(ID1, ID3, REASON, PROCESS, "2023-10-11", "2023-11-11"),
+            Nomination(ID2, "4", REASON, PROCESS, "2023-10-11", "2023-11-11"),
+        )
 
-        assertEquals(expected,result);
+        assertEquals(expected, result);
     }
 
     @Test
-    fun getNomineesTest() {
-        // TODO: Write a test for getting all the nominees from the mock api
+    fun getNomineesTest() = runBlocking {
+        val result = Repository(MockApiService()).getAllNominees()
+        val expected = listOf(
+            Nominee(ID1, "FirstTest1", "LastTest1"),
+            Nominee(ID2, "FirstTest2", "LastTest2"),
+            Nominee(ID3, "FirstTest3", "LastTest3"),
+        )
+
+        assertEquals(expected, result);
     }
 
     @Test
-    fun createNominationTest() {
+    fun createNominationTest()  = runBlocking{
         // TODO: Write a test for creating a new nomination using the mock api
+        val result = Repository(MockApiService()).createNomination(ID1, REASON, PROCESS)
+        val expected = Nomination("3",ID1, REASON, PROCESS,"2023-10-11","2024-04-11");
+        assertEquals(expected, result);
     }
 }
--- app/src/main/java/com/cube/cubeacademy/lib/di/Repository.kt
+++ app/src/main/java/com/cube/cubeacademy/lib/di/Repository.kt
@@ -3,23 +3,34 @@ package com.cube.cubeacademy.lib.di
 import com.cube.cubeacademy.lib.api.ApiService
 import com.cube.cubeacademy.lib.models.Nomination
 import com.cube.cubeacademy.lib.models.Nominee
+import java.time.LocalDate
+import java.util.stream.IntStream
 
 class Repository(val api: ApiService) {
-	// TODO: Add additional code if you need it
+    // TODO: Add additional code if you need it
 
-	suspend fun getAllNominations(): List<Nomination> {
-		// TODO: Write the code to fetch the list nominations from the api
-		return api.getAllNominations().data
-//		return emptyList()
-	}
+    suspend fun getAllNominations(): List<Nomination> {
+        return api.getAllNominations().data
+    }
 
-	suspend fun getAllNominees(): List<Nominee> {
-		// TODO: Write the code to fetch list of all nominees from the api
-		return emptyList()
-	}
+    suspend fun getAllNominees(): List<Nominee> {
+        return api.getAllNominees().data;
+    }
 
-	suspend fun createNomination(nomineeId: String, reason: String, process: String): Nomination? {
-		// TODO: Write the code to create a new nomination using the api
-		return null
-	}
+    suspend fun createNomination(nomineeId: String, reason: String, process: String): Nomination? {
+        // TODO: Write the code to create a new nomination using the api
+        // TODO: change Close date to the end of the month created instead of an added month.
+        // I Chose to increment the closing date by a month of the day created. As these are monthly employee nominations.
+        // I set that are a variable in case microsecond discrepancy causes issue
+        val createDate = LocalDate.now()
+        val newNominationId = api.getAllNominations().data.last().nominationId.toInt().plus(1); //done this way to increment based of last in api list of nominations.
+        return Nomination(
+            newNominationId.toString(),
+            nomineeId,
+            reason,
+            process,
+            createDate.toString(),
+            createDate.plusMonths(1).toString()
+        )
+    }
 }
