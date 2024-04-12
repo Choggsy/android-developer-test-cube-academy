53d87bd515610b6cb9066cf1795724f9bff9a78c
Chloe <cgreenstreet01@gmail.com>
2024-04-12 17:58:54 +0100

    Connecting createNominationButton on 01.00 to 02.00

--- app/src/main/java/com/cube/cubeacademy/activities/MainActivity.kt
+++ app/src/main/java/com/cube/cubeacademy/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cube.cubeacademy.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.cube.cubeacademy.databinding.ActivityMainBinding
 import com.cube.cubeacademy.lib.di.Repository
@@ -9,28 +11,35 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-	private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var makeNominationButton: Button
 
+    @Inject
+    lateinit var repository: Repository
 
-	@Inject
-	lateinit var repository: Repository
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+		makeNominationButtonListener()
+        populateUI()
+    }
 
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		setContentView(binding.root)
-
-		populateUI()
+	private fun makeNominationButtonListener() {
+		makeNominationButton = binding.createButton
+		makeNominationButton.setOnClickListener {
+			startActivity(Intent(this, CreateNominationActivity::class.java))
+		}
 	}
 
 	private fun populateUI() {
-		/**
-		 * TODO: Populate the UI with data in this function
-		 * 		 You need to fetch the list of user's nominations from the api and put the data in the recycler view
-		 * 		 And also add action to the "Create new nomination" button to go to the CreateNominationActivity
-		 */
-		//suspend issue. research.
-		//Psedo : repository.getAllNominations() if List.empty() -> load 01.00 homescreen. Else use recycler to populate the UI.
-	}
+        /**
+         * TODO: Populate the UI with data in this function
+         * 		 You need to fetch the list of user's nominations from the api and put the data in the recycler view
+         * 		 And also add action to the "Create new nomination" button to go to the CreateNominationActivity
+         */
+        //suspend issue. research.
+        //Psedo : repository.getAllNominations() if List.empty() -> load 01.00 homescreen. Else use recycler to populate the UI.
+    }
 }
