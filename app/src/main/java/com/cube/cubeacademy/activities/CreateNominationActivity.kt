72227bf99b275bb5ea38794780ba03d10ec9bfd3
Chloe <cgreenstreet01@gmail.com>
2024-04-14 15:10:46 +0100

    test refactors

--- app/src/main/java/com/cube/cubeacademy/activities/CreateNominationActivity.kt
+++ app/src/main/java/com/cube/cubeacademy/activities/CreateNominationActivity.kt
@@ -2,6 +2,8 @@ package com.cube.cubeacademy.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +13,12 @@ import com.cube.cubeacademy.lib.di.Repository
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
 class CreateNominationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateNominationBinding
     private lateinit var backButton: Button
+    private lateinit var submitButton: Button
     private lateinit var radioButton: RadioButton
 
     @Inject
@@ -22,11 +26,14 @@ class CreateNominationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityCreateNominationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        DropDownNameHandler(repository).populateDropdownWithNames(binding, this)
         backButtonListener()
+
+        radioButtonListener()
+        reasonTextListener()
+
         populateUI()
     }
 
@@ -35,8 +42,15 @@ class CreateNominationActivity : AppCompatActivity() {
          * TODO:
          * - Add the logic for the views / maybe done
          * - add the logic to create the new nomination using the api
+         *  android:enabled="false" -> true WHEN  all submitEnable -> true THEN startActivity4
+         *
+         *  onClick for submit THen set value (could be private with status fed in)
          */
-        DropDownNameHandler(repository).populateDropdownWithNames(binding,this)
+        submitButton = binding.submitButton
+
+        if (reasonTextListener() == true) {
+            submitButton.isEnabled = true
+        }
     }
 
     private fun backButtonListener() {
@@ -48,8 +62,22 @@ class CreateNominationActivity : AppCompatActivity() {
 
     private fun radioButtonListener() {
         binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-            //setSomething to indicate submit button can be pressed
             radioButton = findViewById(checkedId)
         }
+    }
+
+    private fun reasonTextListener(): Boolean {
+        var reasonSubmitReady = false
+        binding.message.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(arg0: Editable) {
+                reasonSubmitReady = true
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                reasonSubmitReady = false
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+        return reasonSubmitReady
     }
 }
