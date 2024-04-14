3842e914eedb33b7df42c11963c69bb0ee9a4c9d
Chloe <cgreenstreet01@gmail.com>
2024-04-14 18:22:45 +0100

    gradle updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,11 +52,14 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
@@ -67,7 +70,6 @@ dependencies {
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.48")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
