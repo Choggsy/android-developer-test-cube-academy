efcbd20f7d328e28b12e2735b841dbebbbf71900
Chloe <cgreenstreet01@gmail.com>
2024-04-14 22:39:25 +0100

    added logic for nomination Submitted buttons

--- app/src/main/java/com/cube/cubeacademy/activities/NominationSubmittedActivity.kt
+++ app/src/main/java/com/cube/cubeacademy/activities/NominationSubmittedActivity.kt
@@ -1,24 +1,37 @@
 package com.cube.cubeacademy.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.cube.cubeacademy.databinding.ActivityNominationSubmittedBinding
 
 class NominationSubmittedActivity : AppCompatActivity() {
-	private lateinit var binding: ActivityNominationSubmittedBinding
+    private lateinit var binding: ActivityNominationSubmittedBinding
+    private lateinit var makeNominationButton: Button
+    private lateinit var backButton: Button
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-		binding = ActivityNominationSubmittedBinding.inflate(layoutInflater)
-		setContentView(binding.root)
+        binding = ActivityNominationSubmittedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-		populateUI()
-	}
+        makeNominationButtonListener()
+        backButtonListener()
+    }
 
-	private fun populateUI() {
-		/**
-		 * TODO: Add the logic for the two buttons (Don't forget that if you start to add a new nomination, the back button shouldn't come back here)
-		 */
-	}
+    private fun makeNominationButtonListener() {
+        makeNominationButton = binding.submitButton
+        makeNominationButton.setOnClickListener {
+            startActivity(Intent(this, CreateNominationActivity::class.java))
+        }
+    }
+
+    private fun backButtonListener() {
+        backButton = binding.backButton
+        backButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
 }
